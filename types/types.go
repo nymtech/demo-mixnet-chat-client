1d7f10f4b1a3ce2d110ff052d246060539f66b3d
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-10-10 17:13:57 +0100

    session storing current recipient alias

--- types/types.go
+++ types/types.go
@@ -4,12 +4,21 @@ package types
 import "github.com/nymtech/nym-mixnet/config"
 
 type Session struct {
-	recipient    config.ClientConfig
-	sessionNonce int64
+	recipient      config.ClientConfig
+	recipientAlias string
+	sessionNonce   int64
 }
 
 func (s *Session) Recipient() config.ClientConfig {
 	return s.recipient
+}
+
+func (s *Session) UpdateAlias(alias string) {
+	s.recipientAlias = alias
+}
+
+func (s *Session) RecipientAlias() string {
+	return s.recipientAlias
 }
 
 func (s *Session) IncrementNonce() int64 {
@@ -17,10 +26,10 @@ func (s *Session) IncrementNonce() int64 {
 	return s.sessionNonce
 }
 
-func NewSession(recipient config.ClientConfig) *Session {
+func NewSession(recipient config.ClientConfig, alias string) *Session {
 	return &Session{
-		recipient:    recipient,
-		sessionNonce: 0,
+		recipient:      recipient,
+		recipientAlias: alias,
+		sessionNonce:   0,
 	}
 }
-
