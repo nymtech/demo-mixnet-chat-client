37234d3fb512de00aeffb8d4b61a400b1d00f8af
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-10-10 15:45:19 +0100

    show handler

--- chat-client/commands/alias/alias.go
+++ chat-client/commands/alias/alias.go
@@ -3,21 +3,49 @@ package alias
 import (
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"github.com/jroimartin/gocui"
 	"github.com/nymtech/demo-mixnet-chat-client/chat-client/commands"
+	"github.com/nymtech/demo-mixnet-chat-client/gui"
 	"github.com/nymtech/demo-mixnet-chat-client/types"
 	"github.com/nymtech/nym-mixnet/sphinx"
+)
+
+const (
+	noAlias = "<none>"
+	commandName = "alias"
+	showSubCommand = "show"
+	removeSubCommand = "remove"
+	addSubCommand = "add"
+	allModifier = "all"
 )
 
 var (
 	ErrNotEnoughArguments = errors.New("alias command did not receive enough arguments")
 	ErrInvalidArguments = errors.New("alias command received invalid arguments")
-	ErrGeneric = errors.New("could not handle alias command")
+	ErrMalformedRecipient = 			 errors.New("malformed recipient data")
+ErrGeneric = errors.New("could not handle alias command")
 
 	forbiddenAliases = []string{
-		"all",
+		noAlias,
+		commandName,
+		showSubCommand,
+		removeSubCommand,
+		addSubCommand,
+		allModifier,
 	}
 )
+
+
+type AliasStore interface {
+	StoreAlias(alias *Alias)
+	GetAlias(*sphinx.PublicKey, *sphinx.PublicKey) *Alias
+	RemoveAlias(alias *Alias)
+	RemoveAliasByKeys(*sphinx.PublicKey, *sphinx.PublicKey)
+	GetAllAliasesByName(string) []*Alias
+	GetAllAliases() []*Alias
+	RemoveAllAliases()
+}
 
 type Alias struct {
 	AssignedName      string
@@ -25,19 +53,24 @@ type Alias struct {
 	ProviderPublicKey *sphinx.PublicKey
 }
 
-type AliasStore interface {
-	StoreAlias(alias *Alias)
-	GetAlias(*sphinx.PublicKey, *sphinx.PublicKey) *Alias
-	RemoveAlias(alias *Alias)
-	RemoveAliasByKeys(*sphinx.PublicKey, *sphinx.PublicKey)
-	GetAllAliases() []*Alias
-	RemoveAllAliases()
+func (a *Alias) String() string {
+	b64Key := base64.URLEncoding.EncodeToString(a.PublicKey.Bytes())
+	b64ProvKey := base64.URLEncoding.EncodeToString(a.ProviderPublicKey.Bytes())
+	assignedName := a.AssignedName
+	if assignedName == "" {
+		assignedName = noAlias
+	}
+	return fmt.Sprintf("Alias: %s - Public Key: %s Provider's Public Key: %s", assignedName, b64Key, b64ProvKey)
 }
+
 
 type AliasCmd struct {
 	g     *gocui.Gui
 	store AliasStore
 	session *types.Session
+
+	// TODO: possible set of subcommands? so separate explicit handlers for remove, add, show, etc
+	//subCommands []commands.Command
 }
 
 
@@ -75,24 +108,39 @@ func (a *AliasCmd) getTargetKeysFromStrings(targetKey, targetProvKey string) (*s
 
 	return a.keysFromBytes(targetKeyB, targetProvKeyB)
 }
+//
+//func (a *AliasCmd) mustGetMessagesView() *gocui.View {
+//	v, err := a.g.View(layout.InputViewName)
+//	if err != nil {
+//		panic(err)
+//	}
+//	return v
+//}
 
-
+func checkIfValidName(name string) bool {
+	for _, invalidAlias := range forbiddenAliases {
+		if name == invalidAlias {
+			return false
+		}
+	}
+	return true
+}
 
 func (a *AliasCmd) Name() string {
-	return "alias"
+	return commandName
 }
 
 func (a *AliasCmd) Usage() string {
 	usageString := "\n"
-	usageString += "\t/alias: \n"
-	usageString += "\t\t - /alias add <aliased_named>\n"
-	usageString += "\t\t - /alias add <b64_public_key> <b64_provider_public_key> <aliased_name>\n"
-	usageString += "\t\t - /alias remove\n"
-	usageString += "\t\t - /alias remove <b64_public_key> <b64_provider_public_key>\n"
-	usageString += "\t\t - /alias remove all\n"
-	usageString += "\t\t - /alias show\n"
-	usageString += "\t\t - /alias show <aliased_name>\n"
-	usageString += "\t\t - /alias show all\n"
+	usageString += fmt.Sprintf("\t/%s: \n", commandName)
+	usageString += fmt.Sprintf("\t\t - /%s %s <aliased_named>\n", commandName, addSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s <b64_public_key> <b64_provider_public_key> <aliased_name>\n", commandName, addSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s\n", commandName, removeSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s <b64_public_key> <b64_provider_public_key>\n", commandName, removeSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s all\n", commandName, removeSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s\n", commandName, showSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s <aliased_name>\n", commandName, showSubCommand)
+	usageString += fmt.Sprintf("\t\t - /%s %s all\n", commandName, showSubCommand)
 	return usageString
 }
 
@@ -110,10 +158,10 @@ func (a *AliasCmd) handleRemove(args []string) error {
 			a.store.RemoveAliasByKeys(currentPub, currentProvPub)
 			return nil
 		} else {
-			return errors.New("malformed recipient data")
+			return ErrMalformedRecipient
 		}
 	case 2:
-		if args[1] == "all" {
+		if args[1] == allModifier {
 			a.store.RemoveAllAliases()
 			return nil
 		} else {
@@ -141,6 +189,9 @@ func (a *AliasCmd) handleAdd(args []string) error {
 	case 1:
 		return ErrNotEnoughArguments
 	case 2:
+		if !checkIfValidName(args[1]) {
+			return ErrInvalidArguments
+		}
 		currentPub, currentProvPub := a.getCurrentRecipientKeys()
 		if currentPub != nil && currentProvPub != nil {
 			alias := &Alias{
@@ -154,6 +205,9 @@ func (a *AliasCmd) handleAdd(args []string) error {
 			return errors.New("malformed recipient data")
 		}
 	case 4:
+		if !checkIfValidName(args[3]) {
+			return ErrInvalidArguments
+		}
 		targetKey, targetProvKey := a.getTargetKeysFromStrings(args[1], args[2])
 		if targetKey != nil && targetProvKey != nil {
 			alias := &Alias{
@@ -179,6 +233,31 @@ func (a *AliasCmd) handleShow(args []string) error {
 	// first element in the slice is the name of the command itself and always exists
 	switch len(args) {
 	case 1:
+		currentPub, currentProvPub := a.getCurrentRecipientKeys()
+		if currentPub != nil && currentProvPub != nil {
+			currentAlias := a.store.GetAlias(currentPub, currentProvPub)
+			gui.WriteInfo(currentAlias.String()+"\n", a.g, "alias_info")
+			return nil
+		}
+		return ErrMalformedRecipient
+	case 2:
+		var aliases []*Alias
+		if args[1] == allModifier {
+			aliases = a.store.GetAllAliases()
+			if len(aliases) == 0 {
+				gui.WriteInfo("no aliases assigned", a.g, "alias_info")
+			}
+		} else {
+			aliases = a.store.GetAllAliasesByName(args[1])
+			if len(aliases) == 0 {
+				gui.WriteInfo(fmt.Sprintf("no clients with alias: %s", args[1]), a.g, "alias_info")
+			}
+		}
+
+		for _, alias := range aliases {
+			gui.WriteInfo(alias.String()+"\n", a.g, "alias_info")
+		}
+		return nil
 
 	default:
 		return ErrInvalidArguments
@@ -190,14 +269,19 @@ func (a *AliasCmd) Handle(args []string) error {
 	if len(args) == 1 {
 		return ErrNotEnoughArguments
 	}
+	// sanity check
+	if args[0] != commandName {
+		return fmt.Errorf("invalid handler called. Expected: %s. got: %s", a.Name(), args[0])
+	}
 	switch args[1] {
-	case "add", "new":
+	case addSubCommand:
 		return a.handleAdd(args[1:])
-	case "rm", "remove", "delete":
+	case removeSubCommand:
 		return a.handleRemove(args[1:])
-	case "show", "display":
+	case showSubCommand:
 		return a.handleShow(args[1:])
 	default:
+		fmt.Println(args[1])
 		return ErrInvalidArguments
 	}
 }
